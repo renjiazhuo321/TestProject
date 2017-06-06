2715ada38874e084c38207793a4e038e8e64388e
unknown <jiazhuo.ren@transsion.com>
2017-06-06 18:28:39 +0800

    更新lib

--- MyApplication/testlibrary/src/main/java/ui/zhuo/com/testlibrary/TestUtils.java
+++ MyApplication/testlibrary/src/main/java/ui/zhuo/com/testlibrary/TestUtils.java
@@ -11,4 +11,7 @@ public class TestUtils {
     public static void  showShortToast(Context context,String text){
         Toast.makeText(context,text,Toast.LENGTH_SHORT).show();
     }
+    public static void  showLongToast(Context context,String text){
+        Toast.makeText(context,text,Toast.LENGTH_LONG).show();
+    }
 }
